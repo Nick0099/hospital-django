58b087f5da764ab20d459dba7f9d129ff8402845
Nischal <nischalneupane45@gmail.com>
2026-06-14 17:21:33 +0545

    fixed prescription_view

--- hospital/settings.py
+++ hospital/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%w9p*b@oavxeuu65k7k$!!+k-43ie&=sal&--@u9tjd%5xn%z%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
